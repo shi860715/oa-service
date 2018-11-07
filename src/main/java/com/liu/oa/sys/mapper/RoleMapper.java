71b6da384ef6559cca77d1a30cebbb492047ddad
Administrator <Administrator@M0RZMF631IZ6H8Z>
2018-11-07 11:14:06 +0800

    修改接口文档备注

--- src/main/java/com/liu/oa/sys/mapper/RoleMapper.java
+++ src/main/java/com/liu/oa/sys/mapper/RoleMapper.java
@@ -10,10 +10,24 @@ import com.liu.oa.sys.model.RoleMenu;
 @Mapper
 public interface RoleMapper extends BaseMapper<Role> {
 
+	/**
+	 * 
+	 * @param rolemenu
+	 * 
+	 * 用来保存角色和资源的关系集合
+	 * 
+	 */
 	void saveRoleMenu(RoleMenu rolemenu);
-
+    /**
+     * 通过角色的id 返回资源id的集合
+     * @param roleId
+     * @return
+     */
 	List<Integer> getMenuIdByRoleId(Integer roleId);
-
+    /**
+     * 根据角色的id 来清除角色和资源之间的对应关系
+     * @param roleId
+     */
 	void deleteRoleMenuByRoleId(Integer roleId);
 
 
