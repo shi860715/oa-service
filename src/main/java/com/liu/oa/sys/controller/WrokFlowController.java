b34e3549fa135153c7f67d29970ee138ce1403dc
Administrator <Administrator@M0RZMF631IZ6H8Z>
2018-11-19 14:55:54 +0800

    更新请假

--- src/main/java/com/liu/oa/sys/controller/WrokFlowController.java
+++ src/main/java/com/liu/oa/sys/controller/WrokFlowController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.activiti.engine.repository.Deployment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import com.liu.oa.framwork.utils.JacksonUtil;
 import com.liu.oa.sys.service.WorkFlowService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -58,13 +60,17 @@ public class WrokFlowController {
 	
 	
 	@RequestMapping("/addDeployment")
+	@ResponseBody
 	 public Map<String,Object> addDeployment(HttpServletRequest request ,MultipartFile file) {
 		Map<String,Object> result = new HashMap<>();
 		String fileName=request.getParameter("fileName");
-		 CommonsMultipartFile cFile =(CommonsMultipartFile) file;
+	
 		  try {
-			  InputStream inputStream=cFile.getInputStream();
-			  wrokFlowService.addDeploy(fileName,inputStream);
+			  InputStream inputStream = file.getInputStream();
+			  Deployment d= wrokFlowService.addDeploy(fileName,inputStream);
+			  log.info("xxxx{}",JacksonUtil.printJson(d));
+			  
+			  
 			  result.put("code", 1);
 			  result.put("message", "流程部署成功");
 			  
