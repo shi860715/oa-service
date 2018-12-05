2b3847a9e45719bde02e5d0d82561de996e5044f
Administrator <Administrator@M0RZMF631IZ6H8Z>
2018-12-05 16:56:04 +0800

    报销管理

---
+++ src/main/java/com/liu/oa/sys/service/impl/CostPayServiceImpl.java
@@ -0,0 +1,17 @@
+package com.liu.oa.sys.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.liu.oa.sys.model.CostPay;
+import com.liu.oa.sys.service.CostPayService;
+
+@Service("costPayService")
+public class CostPayServiceImpl extends BaseServiceImpl<CostPay> implements CostPayService{
+
+	
+	
+	
+	
+	
+	
+}
